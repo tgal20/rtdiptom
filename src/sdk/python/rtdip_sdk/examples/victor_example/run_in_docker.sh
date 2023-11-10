c2713c7b8de6d2bb0e0c2f7d036f49abeb690e7f
tgal20 <tom.lewis102@gmail.com>
2023-11-10 15:11:49 +0000

    Revert "added docker-compose and script to run it"
    This reverts commit a3f0218eb1f21bfcd7ad724f74eb52335bcb80ef.

--- src/sdk/python/rtdip_sdk/examples/victor_example/run_in_docker.sh
+++ src/sdk/python/rtdip_sdk/examples/victor_example/run_in_docker.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 docker container stop rtdip
 docker container rm rtdip
-#docker container stp rtdip_postgres
-#docker container rm rtdip_postgres
 docker system prune -a -f
 docker image rm  "rtdip:Dockerfile"
 docker build -t "rtdip:Dockerfile" .
 docker run --name rtdip --publish 8080:8080 --publish 3000:3000 "rtdip:Dockerfile"
-#docker image rm "rtdip_postgres:Dockerfile"
-#docker build -t "rtdip_postgres:Dockerfile" ./postgres
-#docker run -d --name rtdip_postgres_container -p 5432:5432 rtdip_postgres
